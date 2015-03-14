5c8c8ccacaf1f6b7273d4f8622467dfc7d666ada
Eder Figueroa <eder.figueroa@outlook.com>
2015-03-13 21:33:40 -0500

    Creacion de clases paara PRODUCTO, empezando frmBuscarProducto

---
+++ Entidades/Categoria.vb
@@ -0,0 +1,11 @@
+﻿Public Class Categoria
+    Public Property Codigo As Integer
+    Public Property Descripcion As String
+    Public Property Vigencia As Boolean
+
+    Public ReadOnly Property NombreCategoria As String
+        Get
+            Return Descripcion
+        End Get
+    End Property
+End Class
